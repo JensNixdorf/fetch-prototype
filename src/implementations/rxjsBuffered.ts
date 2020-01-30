3fdc1d468b8ea0e998b5cabe6cdc8337bf06c75d
JensNixdorf <mopore.org@gmail.com>
2020-01-30 21:42:40 +0100

    Missing buffering added

--- src/implementations/rxjsBuffered.ts
+++ src/implementations/rxjsBuffered.ts
@@ -1,19 +1,29 @@
 import fetch from "node-fetch";
-import { from, Observable } from "rxjs";
-import { flatMap, map, toArray, bufferCount, switchMap, catchError } from "rxjs/operators";
+import { from, Observable, defer } from "rxjs";
+import { toArray, bufferCount, catchError, flatMap, map } from "rxjs/operators";
 import { Item } from "../Item";
 
-const createFetchObservable = (url: string): Observable<Item> => {
-  const promise = fetch(url)
-    .then(response => response.json())
-    .then(json => {
-      const i = {
-        id: json.id,
-        title: json.title  
-      }
-      return i;
-    })
-    return from(promise);
+const createFetchObservable = (urls: string[]): Observable<Item[]> => {
+  return defer( async (): Promise<Item[]> => {
+
+    let promises: Promise<Item>[] = [];
+
+    urls.forEach(url => {
+      const promise = fetch(url)
+      .then(response => response.json())
+      .then(json => {
+        const i = {
+          id: json.id,
+          title: json.title  
+        }
+        return i;
+      });
+      promises = [...promises, promise];
+    });
+    const items = await Promise.all( promises );
+    return items;
+  });
+
 }
 
 const MAX_CONCURRENT_REQUESTS = 10;
@@ -21,21 +31,18 @@ const MAX_CONCURRENT_REQUESTS = 10;
 const fetchRxjsBufferd = async (fetchUrls: string[]): Promise<Item[]> => {
   const items = await from( fetchUrls).pipe(
     bufferCount(MAX_CONCURRENT_REQUESTS),
-    switchMap( urls => {
+    map( urls => {
       console.log(`Batch of ${urls.length} received`);
-      return from( urls);
+      return createFetchObservable(urls);
     }),
-    map( url => createFetchObservable(url) ),
-    flatMap( item => { 
-      return item;
-    }),
+    flatMap(itemsObservable => from(itemsObservable)),
+    flatMap(items => from(items)),
     toArray(),
     catchError(error => {
       console.error(`Something went wrong: ${error}`)
       return from([]);
     })
   ).toPromise();
-
   console.log(`Items fetched: ${items.length}`);
   return items;
 }
